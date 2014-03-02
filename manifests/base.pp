cb1c37ad6785cdf1513233ec285e8545e98658e8
mh <mh@immerda.ch>
2014-03-02 22:37:51 +0100

    cgit cache might be on a disk subsystem

--- manifests/base.pp
+++ manifests/base.pp
@@ -5,25 +5,26 @@ class cgit::base {
 
   file{
     '/etc/cgitrc':
-      source        => [
+      source       => [
         "puppet:///modules/site_cgit/config/${::fqdn}/cgitrc",
         'puppet:/(/modules/site_cgit/config/cgitrc',
         'puppet:///modules/cgit/config/cgitrc',
       ],
-      require       => Package['cgit'],
-      before        => Service['apache'],
-      owner         => root,
-      group         => 0,
-      mode          => '0644';
+      require      => Package['cgit'],
+      before       => Service['apache'],
+      owner        => root,
+      group        => 0,
+      mode         => '0644';
     '/var/cache/cgit':
-      ensure        => 'directory',
-      require       => Package['cgit'],
-      purge         => true,
-      recurse       => true,
-      force         => true,
-      recurselimit  => 1,
-      owner         => apache,
-      group         => apache,
-      mode          => '0644';
+      ensure       => 'directory',
+      require      => Package['cgit'],
+      purge        => true,
+      recurse      => true,
+      force        => true,
+      recurselimit => 1,
+      ignore       => 'lost+found',
+      owner        => apache,
+      group        => apache,
+      mode         => '0644';
   }
 }
