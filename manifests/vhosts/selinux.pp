d41dacf2f2e396ccba7f202091cbc5560bd43f8a
mh <mh@immerda.ch>
2018-10-30 15:44:55 +0100

    linting

--- manifests/vhosts/selinux.pp
+++ manifests/vhosts/selinux.pp
@@ -1,6 +1,6 @@
 # selinux related things
 class cgit::vhosts::selinux {
-  include gitolite::daemon::selinux::web
+  include ::gitolite::daemon::selinux::web
 
   if versioncmp($facts['os']['release']['major'],'7') >= 0 {
     $source = 'puppet:///modules/cgit/selinux/gitolite'
